24af83e129b5f0db4c6fda214ce5bb2cd4edc64d
Tommer Haugland <tommerhaugland@gmail.com>
2021-11-08 15:14:05 +0100

    Lagt readme

--- src/components/Readme.js
+++ src/components/Readme.js
@@ -3,13 +3,26 @@ import React from "react";
 export default function Readme() {
   return (
     <main>
-      <p>
-        # Stacc code challenge 2021 ## Oppgavebeskrivelse Her kan beskrive
-        hvilke oppgave(r) du har valgt å løse. > Bekriv prosjektet ditt kort. ##
-        Hvordan kjøre prosjektet For eksempel ***yarn start*** > Husk å pakke
-        med alle avhengigheter. ## Kommentarer Noen spesielle valg du ønsker å
-        beskrive/forsvare? > Eventuellt andre kommentarer / utfordringer?
-      </p>
+      <ul>
+        <li>
+          {" "}
+          # Getting Started with Create React App This project was bootstrapped
+          with [Create React App](https://github.com/facebook/create-react-app).
+        </li>
+        <li>
+          {" "}
+          ## Available Scripts In the project directory, you can run: ### `npm
+          start` Runs the app in the development mode.\ Open
+          [http://localhost:3000](http://localhost:3000) to view it in the
+          browser.
+        </li>
+        <li>Laget med React-table og tailwind for table ui</li>
+        <li>
+          ## Løsningsforslag 1 - PEP Sjekk av en person Informasjonen hentet fra
+          Stacc API Lister informasjonen som brukeren skriver inn lister basert
+          på navn, informasjonen blir listet ut i sorteringsvennlig tabell
+        </li>
+      </ul>
     </main>
   );
 }
